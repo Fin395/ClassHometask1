d7537889749c24599452581a1c24078f18a21812
Sergei Finogenov <serfinogenov@yandex.ru>
2025-03-04 02:38:30 +0300

    commit creating fixtures and test

--- src/main.py
+++ src/main.py
@@ -26,6 +26,7 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
+
 if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
@@ -46,9 +47,11 @@ if __name__ == "__main__":
     print(product3.price)
     print(product3.quantity)
 
-    category1 = Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                         [product1, product2, product3])
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
 
     print(category1.name == "Смартфоны")
     print(category1.description)
@@ -56,10 +59,12 @@ if __name__ == "__main__":
     print(category1.category_count)
     print(category1.product_count)
 
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category2 = Category("Телевизоры",
-                         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-                         [product4])
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category2 = Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
 
     print(category2.name)
     print(category2.description)
---
+++ tests/test_main.py
@@ -0,0 +1,45 @@
+def test_init_first_product(first_product):
+    assert first_product.name == "Samsung Galaxy S23 Ultra"
+    assert first_product.description == "256GB, Серый цвет, 200MP камера"
+    assert first_product.price == 180000.0
+    assert first_product.quantity == 5
+
+
+def test_init_second_product(second_product):
+    assert second_product.name == "Iphone 15"
+    assert second_product.description == "512GB, Gray space"
+    assert second_product.price == 210000.0
+    assert second_product.quantity == 8
+
+
+def test_init_third_product(third_product):
+    assert third_product.name == "Xiaomi Redmi Note 11"
+    assert third_product.description == "1024GB, Синий"
+    assert third_product.price == 31000.0
+    assert third_product.quantity == 14
+
+
+def test_init_forth_product(forth_product):
+    assert forth_product.name == '55" QLED 4K'
+    assert forth_product.description == "Фоновая подсветка"
+    assert forth_product.price == 123000.0
+    assert forth_product.quantity == 7
+
+
+def test_init_category(first_category, second_category):
+    assert first_category.name == "Смартфоны"
+    assert (
+        first_category.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    )
+    assert len(first_category.products) == 3
+    assert second_category.name == "Телевизоры"
+    assert (
+        second_category.description
+        == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
+    )
+    assert len(second_category.products) == 1
+    assert first_category.category_count == 2
+    assert second_category.category_count == 2
+    assert first_category.product_count == 4
+    assert second_category.product_count == 4
