5329079316f6d18abd3b56fceefe45d1fbb9d517
Sergei Finogenov <serfinogenov@yandex.ru>
2025-03-04 03:04:08 +0300

    commit adding annotation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,30 @@
 import pytest
-from src.main import Product, Category
+
+from src.main import Category, Product
 
 
 @pytest.fixture
-def first_product():
+def first_product() -> Product:
     return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 
 
 @pytest.fixture
-def second_product():
+def second_product() -> Product:
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 
 
 @pytest.fixture
-def third_product():
+def third_product() -> Product:
     return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
 
 @pytest.fixture
-def forth_product():
+def forth_product() -> Product:
     return Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
 
 
 @pytest.fixture
-def first_category():
+def first_category() -> Category:
     return Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
@@ -36,7 +37,7 @@ def first_category():
 
 
 @pytest.fixture
-def second_category():
+def second_category() -> Category:
     return Category(
         "Телевизоры",
         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,32 +1,35 @@
-def test_init_first_product(first_product):
+from src.main import Category, Product
+
+
+def test_init_first_product(first_product: Product) -> None:
     assert first_product.name == "Samsung Galaxy S23 Ultra"
     assert first_product.description == "256GB, Серый цвет, 200MP камера"
     assert first_product.price == 180000.0
     assert first_product.quantity == 5
 
 
-def test_init_second_product(second_product):
+def test_init_second_product(second_product: Product) -> None:
     assert second_product.name == "Iphone 15"
     assert second_product.description == "512GB, Gray space"
     assert second_product.price == 210000.0
     assert second_product.quantity == 8
 
 
-def test_init_third_product(third_product):
+def test_init_third_product(third_product: Product) -> None:
     assert third_product.name == "Xiaomi Redmi Note 11"
     assert third_product.description == "1024GB, Синий"
     assert third_product.price == 31000.0
     assert third_product.quantity == 14
 
 
-def test_init_forth_product(forth_product):
+def test_init_forth_product(forth_product: Product) -> None:
     assert forth_product.name == '55" QLED 4K'
     assert forth_product.description == "Фоновая подсветка"
     assert forth_product.price == 123000.0
     assert forth_product.quantity == 7
 
 
-def test_init_category(first_category, second_category):
+def test_init_category(first_category: Category, second_category: Category) -> None:
     assert first_category.name == "Смартфоны"
     assert (
         first_category.description
