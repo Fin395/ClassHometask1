3041e63501326e3b4bea01647430dd26f4ac0f60
Sergei Finogenov <serfinogenov@yandex.ru>
2025-03-14 00:58:02 +0300

    commit creating new class "ProductIterator" that will iterate through the objects

---
+++ src/product_iterator.py
@@ -0,0 +1,42 @@
+from typing import Any
+
+from src.category import Category
+from src.product import Product
+
+
+class ProductIterator:
+    """Создаем итератор, с помощью которого можно перебирать товары одной категории"""
+
+    def __init__(self, category_obj: Category):
+        """Инициализируем итератор"""
+        self.category_obj = category_obj
+        self.index = 0
+
+    def __iter__(self) -> "ProductIterator":
+        """Возвращает итератор"""
+        self.index = 0
+        return self
+
+    def __next__(self) -> Any:
+        """Возвращает следующий товар в списке"""
+        if self.index < len(self.category_obj.products_in_list):
+            product = self.category_obj.products_in_list[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration
+
+
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+    iterator = ProductIterator(category1)
+    for i in iterator:
+        print(i)
