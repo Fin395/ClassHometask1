0264d70565b33dfe621cf19326cc2fabe27a09dd
Sergei Finogenov <serfinogenov@yandex.ru>
2025-03-16 01:59:56 +0300

    creating subclass "LawnGrass" to the class "Product"

--- src/smartphone.py
+++ src/smartphone.py
@@ -2,7 +2,7 @@ from src.product import Product
 
 
 class Smartphone(Product):
-    """Создаем класс Smartphone с атрибутами"""
+    """Создаем дочерний класс Smartphone от класса Product """
     def __init__(self, name: str, description: str, price: float, quantity: int, efficiency: float, model: str, memory: float, color: str):
         super().__init__(name, description, price, quantity)
         self.efficiency = efficiency
